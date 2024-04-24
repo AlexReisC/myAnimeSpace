6fdeac944997ce532a7e1b7633643ac8fa8cf9a1
AlexReisC <alex.reis@aluno.ufca.edu.br>
2024-04-24 18:35:42 -0300

    chore: Rename package ro 'repository'

---
+++ src/br/com/myAnimeSpace/repository/Space.java
@@ -0,0 +1,13 @@
+package br.com.myAnimeSpace.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.myAnimeSpace.model.Anime;
+import br.com.myAnimeSpace.model.Manga;
+
+public class Space {
+    List<Anime> animes = new ArrayList<>();
+    List<Manga> mangas = new ArrayList<>();
+    
+}
